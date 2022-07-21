5827616d3aa8893eabf57449434d5e33fc89cf24
Yaren <yarenyuvka@gmail.com>
2022-07-21 23:57:37 +0300

    firstCommit

---
+++ degiskenler.py
@@ -0,0 +1,21 @@
+baslik = 'HABERİNİZ OLSUN' #string
+vade = 12 #integer
+faizOrani1 = 1.47 #float
+faizOrani2 = 1.44
+faizOrani = 1.47
+
+print(baslik)
+print(type(baslik))
+print(type(vade))
+print(type(faizOrani1))
+
+mesaj = "Hoşgeldin"
+musteriAdı = "Engin"
+musteriSoyadı =  "Demiroğ"
+sonucMesaj = mesaj + " " + musteriAdı + " " + musteriSoyadı + "!"
+print(sonucMesaj)
+
+sayi1 = 10
+sayi2 = 20
+print(sayi1 + sayi2)
+print(sonucMesaj)
---
+++ main.py
@@ -0,0 +1,22 @@
+dolarDun = 7.65
+dolarBugun = 7.75
+
+if dolarDun > dolarBugun:
+    print("Azalış oku")
+    print("Bitti")
+elif dolarDun< dolarBugun:
+    print("Artış oku")
+else:
+    print("Eşittir oku")
+    
+print("Bitti")
+
+#if dolarDun<dolarBugun:
+    #print("Artış oku")
+
+#if dolarDun==dolarBugun:
+    #print("Eşittir oku")
+
+
+
+
